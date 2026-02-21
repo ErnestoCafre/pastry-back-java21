43567e1268a2f2088768119be1c68420213a943e
Ernesto Cafre <ernesto-cafre@outlook.com>
2026-02-21 11:11:22 -0300

    config migration and update seed

--- backend/src/main/java/com/malva_pastry_shop/backend/config/DataSeeder.java
+++ backend/src/main/java/com/malva_pastry_shop/backend/config/DataSeeder.java
@@ -34,7 +34,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
-@Profile({ "dev", "prod" })
+@Profile("dev")
 public class DataSeeder implements CommandLineRunner {
 
         private static final Logger log = LoggerFactory.getLogger(DataSeeder.class);
